dafbae417d8dbd5063df4f2decba4624fc54a018
Muhammad Akib Jabed <muhammadakibjabed@gmail.com>
2022-11-10 11:00:52 +0600

    header image added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from '../../images/car_logo_PNG1640.png';
 import './Header.css'
-import Background from '../../images/AUDI CARS.jpg'
+
 
 const Header = () => {
-    const headerStyle={
-        width: "100%",
-        height: "400px",
-        backgroundImage: "url(" + { Background } + ")" 
-    }
+    
     return (
         <div className='header'>
             <div className='logo'>
                 <img src={logo} alt="" />
                 <h3 >Audi Car Lovers Hut</h3>
             </div>
-            <div style={headerStyle} className='header-text'>This is header text</div>
+            <div className='header-text'>
+                <h3>Hey Audi Lovers what's going on?</h3>
+                <h3>This is the place where you can choose your favourite Audi cars.
+                </h3>
+                <h3>You have to choose your most favourite Audi cars with limited budget </h3>
+                <h1>Budget limit: $10M</h1>
+            </div>
         </div>
     );
 };
